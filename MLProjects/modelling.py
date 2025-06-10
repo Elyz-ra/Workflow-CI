f1ecb13a7cce65f659c9d785ceb52d5c93683da2
Elyz-ra <elyziajanarakhansa@gmail.com>
2025-06-11 03:39:43 +0700

    Refactor CI workflow and modelling script; update experiment name, streamline MLflow setup, and suppress XGBoost warnings

--- MLProjects/modelling.py
+++ MLProjects/modelling.py
@@ -6,24 +6,29 @@ import pandas as pd
 from xgboost import XGBClassifier
 from sklearn.metrics import accuracy_score
 
-# Argument untuk jalur folder dataset
+# Fix XGBoost warning
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning, module="xgboost.compat")
+
+# Argument parsing
 parser = argparse.ArgumentParser()
 parser.add_argument("--data_folder", type=str, default="ecommerce_shipping_data_preprocessed")
 args = parser.parse_args()
 
-# Load dataset dari folder inputan
+# Load data
 X_train = pd.read_csv(f"{args.data_folder}/X_train.csv")
 X_test = pd.read_csv(f"{args.data_folder}/X_test.csv")
 y_train = pd.read_csv(f"{args.data_folder}/y_train.csv").values.ravel()
 y_test = pd.read_csv(f"{args.data_folder}/y_test.csv").values.ravel()
 
-# Tracking MLflow lokal
-mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI", "http://127.0.0.1:5001"))
-mlflow.set_experiment("Shipping Delay Prediction")
+# MLflow setup
+mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI", "file://" + os.path.join(os.getcwd(), "mlruns")))
+experiment = mlflow.set_experiment("Shipping-Delay-Prediction")
 
-with mlflow.start_run():
+# Model training
+with mlflow.start_run(experiment_id=experiment.experiment_id):
     mlflow.sklearn.autolog()
-
+    
     model = XGBClassifier(
         n_estimators=100,
         learning_rate=0.1,
@@ -33,11 +38,9 @@ with mlflow.start_run():
         random_state=42
     )
     model.fit(X_train, y_train)
-
+    
     preds = model.predict(X_test)
     acc = accuracy_score(y_test, preds)
-
-    print(f"Akurasi: {acc}")
-
-    # Simpan model ke direktori lokal
+    mlflow.log_metric("accuracy", acc)
+    
     mlflow.sklearn.log_model(model, "model")
