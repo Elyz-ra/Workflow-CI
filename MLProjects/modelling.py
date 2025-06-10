160f78ad8dcfc403cd03dc6bdb391ed8d3ad8e80
Elyz-ra <elyziajanarakhansa@gmail.com>
2025-06-10 23:48:54 +0700

    Add XGBoost model training script for shipping delay prediction
    - Implemented a new Python script for training an XGBoost classifier on preprocessed ecommerce shipping data.
    - Integrated MLflow for experiment tracking and model logging.
    - Added command-line argument to specify the data folder containing training and testing datasets.
    - Included accuracy evaluation of the model on the test set.

---
+++ MLProjects/modelling.py
@@ -0,0 +1,39 @@
+import argparse
+import mlflow
+import mlflow.sklearn
+import pandas as pd
+from xgboost import XGBClassifier
+from sklearn.metrics import accuracy_score
+
+# Argument untuk jalur folder dataset
+parser = argparse.ArgumentParser()
+parser.add_argument("--data_folder", type=str, default="ecommerce_shipping_data_preprocessed")
+args = parser.parse_args()
+
+# Load dataset dari folder inputan
+X_train = pd.read_csv(f"{args.data_folder}/X_train.csv")
+X_test = pd.read_csv(f"{args.data_folder}/X_test.csv")
+y_train = pd.read_csv(f"{args.data_folder}/y_train.csv").values.ravel()
+y_test = pd.read_csv(f"{args.data_folder}/y_test.csv").values.ravel()
+
+# Tracking MLflow lokal
+mlflow.set_tracking_uri("http://127.0.0.1:5001")  # bisa diubah ke env var / remote jika skilled
+mlflow.set_experiment("Shipping Delay Prediction")
+
+with mlflow.start_run():
+    mlflow.sklearn.autolog()
+
+    model = XGBClassifier(
+        n_estimators=100,
+        learning_rate=0.1,
+        max_depth=5,
+        use_label_encoder=False,
+        eval_metric='logloss',
+        random_state=42
+    )
+    model.fit(X_train, y_train)
+
+    preds = model.predict(X_test)
+    acc = accuracy_score(y_test, preds)
+
+    print(f"Akurasi: {acc}")
